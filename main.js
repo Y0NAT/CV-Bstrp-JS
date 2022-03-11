6e32832d5ec36348aac870546cee66230eb5de00
Y0N4TH4N <ezechiel.yonathan.favre@gmail.com>
2022-03-11 16:26:49 +0100

    NoFixJs/commit
    JS inclu mais pas complet. :(

---
+++ main.js
@@ -0,0 +1,23 @@
+// let checkis = document.getElementByID("flexSwitchCheckDefault");
+
+// checkis.addEventListener("click", function () {
+//     //let beaugos = document.querySelectorAll('div[class="bg-secondary"]')
+//     // document.getElementsByClassName("bg-secondary").classList.remove("bg-secondary");
+//     // document.getElementsByClassName("container-fluid").classList.add("bg-light");
+//     document.getElementsByClassName("MyElement").classList.add('MyClass');
+
+//     document.getElementsByClassName("MyElement").classList.remove('MyClass');
+
+//     if (document.getElementsByClassName("MyElement").classList.contains('MyClass'));
+
+//     document.getElementsByClassName("MyElement").classList.toggle('MyClass');
+// })
+
+function toggleCheckbox(){
+
+    document.getElementById("bkgrnd").classList.remove('bg-secondary');
+
+    //  if (document.getElementById("bkgrnd").classList.remove('bg-secondary'));
+
+    //  document.getElementById("bkgrnd").classList.toggle('bg-secondary');
+}
